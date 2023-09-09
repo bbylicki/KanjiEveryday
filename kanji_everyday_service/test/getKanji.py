279260d6e5f81b8f2e56918a05e4c0bfb8cad195
Brian Bylicki <bbylicki@gmail.com>
2023-09-08 21:20:54 -0400

    starting to implement tests

---
+++ kanji_everyday_service/test/getKanji.py
@@ -0,0 +1,19 @@
+import unittest
+from kanji_everyday_service.src.KanjiEverydayServer import app
+
+class TestGetKanjiEndpoint(unittest.TestCase):
+    def assembly(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+    
+    def testEndpoint(self):
+        response = self.app.get('/api/getKanji')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['kanji'], 力)
+        print("hello")
+        self.assertEqual(data['kanji'], a)
+
+if __name__ == '__main__':
+    unittest.main()
+        
